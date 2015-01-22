dc7454b6924fbf0ff64db709af3f6955db933aaa
Umut Seven <umutseven92@gmail.com>
2015-01-22 04:03:12 +0200

    İleri arama ekrani degisti

--- src/Fragments/IncomeSearchFragment.java
+++ src/Fragments/IncomeSearchFragment.java
@@ -5,24 +5,79 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import com.graviton.Cuzdan.R;
+
+import java.util.jar.Attributes;
 
 /**
  * Created by Umut on 14.1.2015.
  */
-public class IncomeSearchFragment extends Fragment{
+public class IncomeSearchFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+
+    View v;
+    Spinner spnSearchCategory,spnSearchSubCategory,spnSearchDate;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View v = inflater.inflate(R.layout.incomesearchfragment, null);
+        v = inflater.inflate(R.layout.incomesearchfragment, null);
+
+        spnSearchCategory = (Spinner)v.findViewById(R.id.spnSearchCategory);
+        spnSearchSubCategory = (Spinner)v.findViewById(R.id.spnSearchSubCategory);
+        spnSearchDate = (Spinner)v.findViewById(R.id.spnSearchDate);
+        spnSearchDate.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> categoryAdapter = ArrayAdapter.createFromResource(v.getContext(), R.array.incomeCategories, android.R.layout.simple_spinner_item);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSearchCategory.setAdapter(categoryAdapter);
+        spnSearchCategory.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> dateAdapter = ArrayAdapter.createFromResource(v.getContext(), R.array.balanceDateArray, android.R.layout.simple_spinner_item);
+        dateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSearchDate.setAdapter(dateAdapter);
+        spnSearchDate.setOnItemSelectedListener(this);
+
+        int id = getResources().getIdentifier("subCategories","array",getActivity().getPackageName());
+/*
+        ArrayAdapter<CharSequence> subCategoryAdapter = ArrayAdapter.createFromResource(v.getContext(), id, android.R.layout.simple_spinner_item);
+        subCategoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSearchSubCategory.setAdapter(subCategoryAdapter);
+*/
         return v;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        int pID = parent.getId();
+        int cID = spnSearchCategory.getId();
+
+        if(pID == cID)
+        {
+            int a = 5;
+        }
+        if(parent.getId() == spnSearchSubCategory.getId())
+        {
+            int a = 5;
+        }
+        if(parent.getId() == spnSearchDate.getId())
+        {
+            int a = 5;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
