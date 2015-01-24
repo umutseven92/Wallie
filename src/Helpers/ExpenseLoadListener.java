aa364c90edf26339252af1abb568483481f290cc
Umut Seven <umutseven92@gmail.com>
2015-01-24 12:35:46 +0200

    Gereksiz classlar silindi

--- src/Helpers/ExpenseLoadListener.java
+++
@@ -1,8 +0,0 @@
-package Helpers;
-
-/**
- * Created by Umut on 18.1.2015.
- */
-public interface ExpenseLoadListener {
-    public void onDismissed();
-}
--- src/Helpers/IncomeLoadListener.java
+++
@@ -1,8 +0,0 @@
-package Helpers;
-
-/**
- * Created by Umut on 18.1.2015.
- */
-public interface IncomeLoadListener {
-    public void onDismissed();
-}
