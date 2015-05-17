ee781b8366d8230eb265fa97383b8c91fb76ca8e
Umut Seven <umutseven92@gmail.com>
2015-05-17 23:00:36 +0300

    Uygulama tabletlere daha uygun hale getirildi

--- src/main/java/Helpers/Billing/IabHelper.java
+++ src/main/java/Helpers/Billing/IabHelper.java
@@ -259,6 +259,12 @@ public class IabHelper {
 
         Intent serviceIntent = new Intent("com.android.vending.billing.InAppBillingService.BIND");
         serviceIntent.setPackage("com.android.vending");
+
+        if(mContext.getPackageManager().queryIntentServices(serviceIntent, 0) == null)
+        {
+            return;
+        }
+
         if (!mContext.getPackageManager().queryIntentServices(serviceIntent, 0).isEmpty()) {
             // service available to handle that Intent
             mContext.bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
